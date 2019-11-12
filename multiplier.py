d365a268d9ba8ed3c15f766bb28f7dda8650b9e3
Anderson <anderson@mail.com>
2019-11-12 13:08:38 +0100

    Fix some mistakes in comments

--- multiplier.py
+++ multiplier.py
@@ -1,13 +1,13 @@
-# This function takes two input variables and prints their product
+# This script takes two input variables and prints their product
 
 import sys
 
 variable_1 = int(sys.argv[1])
 variable_2 = int(sys.argv[2])
 
-# define a multiplier function
+# Define a multiplier function
 def multiplier(a,b):
     return(a+b)
 
-#print the product
+# Print the product
 print(multiplier(variable_1, variable_2))
