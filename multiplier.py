0f601126741018024840e1b6adc91dc362e081cf
Anderson <anderson@mail.com>
2019-11-12 09:52:46 +0100

    first commit, multiplier works

---
+++ multiplier.py
@@ -0,0 +1,9 @@
+# This function takes two input variables and prints their product
+
+import sys
+
+variable_1 = int(sys.argv[1])
+variable_2 = int(sys.argv[2])
+
+#print the product
+print(variable_1 + variable_2)
